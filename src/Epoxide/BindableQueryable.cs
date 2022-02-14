ca161b3c6f2e984a0dab96e6e8debd1cabf6ec8b
Jean-Philippe Leconte <ins0mniaque@gmail.com>
2022-02-13 19:25:08 -0500

    Remove BindableQuery AsQueryable dependency

--- src/Epoxide/BindableQueryable.cs
+++ src/Epoxide/BindableQueryable.cs
@@ -3,16 +3,37 @@ using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 using System.Linq.Expressions;
 
+using Epoxide.Linq;
+
 namespace Epoxide;
 
 public static class BindableEnumerable
 {
     public static IQueryable<TElement> AsBindable<TElement>(this IEnumerable<TElement> source)
     {
-        var q = source.AsQueryable (  );
-        var provider = new BindableQueryProvider(source, q.Provider);
-        return provider.CreateQuery<TElement>(q.Expression);
+        if (source == null)
+            throw Error.ArgumentNull(nameof(source));
+
+        if(source is IQueryable<TElement> queryable && queryable.Provider is BindableQuery)
+            return queryable;
+
+        return new BindableQuery<TElement>(source);
     }
+
+    // public static IQueryable AsBindable(this IEnumerable source)
+    // {
+    //     if (source == null)
+    //         throw Error.ArgumentNull(nameof(source));
+    //
+    //     if (source is IQueryable queryable && queryable.Provider is BindableQuery)
+    //         return queryable;
+    //
+    //     Type? enumType = TypeHelper.FindGenericType(typeof(IEnumerable<>), source.GetType());
+    //     if (enumType == null)
+    //         throw Error.ArgumentNotIEnumerableGeneric(nameof(source));
+    //
+    //     return BindableQuery.Create(enumType.GenericTypeArguments[0], source);
+    // }
 
     public static TCollection ToList <TCollection, TElement>(this IEnumerable<TElement> source)
         where TCollection : ICollection<TElement>, new ( )
@@ -32,15 +53,18 @@ public static class BindableQueryable
         return ToList<ObservableCollection<TElement>, TElement>(source);
     }
 
-
     public static TCollection ToList <TCollection, TElement>(this IQueryable<TElement> source)
         where TCollection : ICollection<TElement>, new ( )
     {
         var output = source.AsEnumerable ( ).ToList <TCollection, TElement> ( );
 
-        if ( source is BindableQuery<TElement> magic )
+        if ( source is BindableQuery<TElement> bindable )
         {
-            if (( (BindableQueryProvider) magic.Provider ).Source is INotifyCollectionChanged ncc )
+            var root = bindable.Expression;
+            while ( root is MethodCallExpression m )
+                root = m.Object ?? m.Arguments [ 0 ];
+
+            if ( root is ConstantExpression c && c.Value is BindableQuery q && q.Enumerable is INotifyCollectionChanged ncc )
             {
                 ncc.CollectionChanged += (o, e) =>
                 {
@@ -52,88 +76,5 @@ public static class BindableQueryable
         }
 
         return output;
-    }
-}
-
-public class BindableQueryProvider : IQueryProvider
-{
-    private readonly IQueryProvider provider;
-    private readonly IEnumerable source;
-
-    public BindableQueryProvider(IEnumerable source, IQueryProvider provider)
-    {
-        this.provider = provider;
-        this.source = source;
-    }
-
-    public IEnumerable Source => source;
-
-    public IEnumerator<TElement> ExecuteQuery<TElement>(Expression expression)
-    {
-        IQueryable<TElement> query = provider.CreateQuery<TElement>(expression);
-        IEnumerator<TElement> enumerator = query.GetEnumerator();
-        return enumerator;
-    }
-
-    public IQueryable<TElement> CreateQuery<TElement>(Expression expression)
-    {
-        IQueryable<TElement> queryable = provider.CreateQuery<TElement>(expression);
-        return new BindableQuery<TElement>(queryable, this);
-    }
-
-    public IQueryable CreateQuery(Expression expression)
-    {
-        IQueryable queryable = provider.CreateQuery(expression);
-        Type elementType = queryable.ElementType;
-        Type queryType = typeof(BindableQuery<>).MakeGenericType(elementType);
-        return (IQueryable)Activator.CreateInstance(queryType, queryable, this);
-    }
-
-    public TResult Execute<TResult>(Expression expression)
-    {
-        return provider.Execute<TResult>(expression);
-    }
-
-    public object Execute(Expression expression)
-    {
-        return provider.Execute(expression);
-    }
-}
-
-public class BindableQuery<TElement> : IQueryable<TElement>
-{
-    private readonly IQueryable queryable;
-    private readonly BindableQueryProvider provider;
-
-    public BindableQuery(IQueryable queryable, BindableQueryProvider provider)
-    {
-        this.queryable = queryable;
-        this.provider = provider;
-    }
-
-    public IEnumerator<TElement> GetEnumerator()
-    {
-        Expression expression = queryable.Expression;
-        return provider.ExecuteQuery<TElement>(expression);
-    }
-
-    IEnumerator IEnumerable.GetEnumerator()
-    {
-        return GetEnumerator();
-    }
-
-    public Type ElementType
-    {
-        get { return typeof(TElement); }
-    }
-
-    public Expression Expression
-    {
-        get { return queryable.Expression; }
-    }
-
-    public IQueryProvider Provider
-    {
-        get { return provider; }
     }
 }
